12328798a91360fd3c49fd43eefdd8714965b4e7
mounikasambareddy <mounika.sambareddy@gmail.com>
2017-03-10 05:59:18 -0500

    success5

--- app/src/main/java/com/example/user1/arraylistobjects/C.java
+++ app/src/main/java/com/example/user1/arraylistobjects/C.java
@@ -17,8 +17,7 @@ public class C extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-Log.d("monika","--->");
-        Log.d("monika","--->");
+        
 
     }
 }
